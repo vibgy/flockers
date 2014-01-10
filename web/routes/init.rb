4d6eb100eb04064cd6af94fd14caa20c9149d6f0
arpita18 <arpita.m.18@gmail.com>
2014-01-11 00:57:22 +0530

    proper indentation

--- web/routes/init.rb
+++ web/routes/init.rb
@@ -3,7 +3,7 @@ require 'data_mapper'
 require 'sinatra'
 require 'haml'
 require 'json'
-#require 'pry'
+require 'pry-debugger'
 
 enable :sessions
 
@@ -11,134 +11,127 @@ get '/' do
 "Hello World"
 end
 
-get '/bar' do
-  haml :ar
-end
-
 get '/home' do
    @he=Event.all
-    haml:home
+   haml:home
 end
 
 get '/events' do
-  @events = Event.all
-  @events.to_json;
+   @events = Event.all
+   @events.to_json;
 end
 
 get '/searchEventByCategory' do
- @hs=Event.all(:category => params[:record])
-    if @hs.any?
-        data = @hs.to_json;
-    else
-        data = {:status => 'Failure'}.to_json;
-    end
-    return data;
+   @hs=Event.all(:category => params[:record])
+   if @hs.any?
+      data = @hs.to_json;
+   else
+      data = {:status => 'Failure'}.to_json;
+   end
+   return data;
 end
 
 post '/login' do 
-    @he =Account.first(:uname => params[:user_name], :password => params[:pass]); 
-    session['user']=params[:user_name];
-    if @he.nil?
-     return {:status => 'not success'}.to_json;
-    else
-    @he.to_json;
-    end
+   @he =Account.first(:uname => params[:user_name], :password => params[:pass]); 
+   session['user']=params[:user_name];
+   if @he.nil?
+      return {:status => 'not success'}.to_json;
+   else
+      @he.to_json;
+   end
 end
 
 post '/signout' do
-    session['user']='';
-    @he=Event.all
-    unless @he.nil?
-        return {:status => 'success'}.to_json;
-    else
-        return {:status => 'not success'}.to_json;
-    end
+   session['user']='';
+   @he=Event.all
+   unless @he.nil?
+      return {:status => 'success'}.to_json;
+   else
+      return {:status => 'not success'}.to_json;
+   end
 end
 
 get '/myEvents.json' do
-    @user = session['user'];
-    @hm=Event.all(:organizer => session['user'])
-    @hm.to_json;
+   @user = session['user'];
+   @hm=Event.all(:organizer => session['user'])
+   @hm.to_json;
 end
 
 get '/publicEvents.json' do
-    @hm=Event.all
-    @hm.to_json;
+   @hm=Event.all
+   @hm.to_json;
 end
 
 get '/participationID.json' do
-    @hm=Participation.all(:user_id => session['user'],:fields => [:id,:event_id])
-    if @hm.any?
-    @hm.to_json;
-    else 
-    {:status => 'Failure'}.to_json;
-    end
-    #binding.pry
+   @hm=Participation.all(:user_id => session['user'],:fields => [:id,:event_id])
+   if @hm.any?
+      @hm.to_json;
+   else 
+      {:status => 'Failure'}.to_json;
+   end
 end
 
 post '/participationEvents.json' do
-    array=params[:event]
-    events = Array.new
-    array.each do |i|
-	  events << Event.first(:id => i.to_i)
-          #binding.pry
-    end
-    return events.to_json;
+   array=params[:event]
+   events = Array.new
+   array.each do |i|
+      events << Event.first(:id => i.to_i)
+   end
+   return events.to_json;
 end
 
 get '/loggedIn' do
-  @user = session['user'];
-  haml :profile
+   @user = session['user'];
+   haml :profile
 end
 
 post '/createEvent' do
-  event = params[:event];
-  status = Event.create(:ename => event[:ename],:date => event[:date],:time => event[:time],:place => event[:place],:organizer => session['user'],:fees => event[:fees],:prize => event[:prize],:description =>event[:description]);
- return status;
+   event = params[:event];
+   status = Event.create(:ename => event[:ename],:date => event[:date],:time => event[:time],:place => event[:place],:organizer => session['user'],:fees => event[:fees],:prize => event[:prize],:description =>event[:description]);
+   return status;
 end
 
 post '/signup' do
-     Account.create(:uname => params[:user_name],:password => params[:pass])
+   Account.create(:uname => params[:user_name],:password => params[:pass])
 end
 
 post '/search' do
-    @hs=Event.all(:ename => params[:record])
-
-    if @hs.any?
-        data = @hs.to_json;
-    else
-        data = {:status => 'Failure'}.to_json;
-    end
-    return data;
+   @hs=Event.all(:ename => params[:record])
+   if @hs.any?
+      data = @hs.to_json;
+   else
+      data = {:status => 'Failure'}.to_json;
+   end
+   return data;
 end
 
 post '/delete' do
-     zoo = Event.first(:id => params[:event_id].to_i)
-     foo = Participation.all(:event_id => params[:event_id].to_i)
-     unless foo.nil?
-     foo.destroy
-     end
-     zoo.destroy
-     if zoo.destroyed?
-     return {:status => 'success'}.to_json;
-     else
-     return {:status => 'not_success'}.to_json; 
-     end
+   zoo = Event.first(:id => params[:event_id].to_i)
+   foo = Participation.all(:event_id => params[:event_id].to_i)
+   unless foo.nil?
+      foo.destroy
+   end
+   zoo.destroy
+   if zoo.destroyed?
+      return {:status => 'success'}.to_json;
+   else
+      return {:status => 'not_success'}.to_json; 
+   end
 end
 
 post '/deleteParticipationEvent' do
-     zoo = Participation.first(:event_id => params[:event_id].to_i)
-     zoo.destroy
-     if zoo.destroyed?
-     return {:status => 'success'}.to_json;
-     else
-     return {:status => 'not_success'}.to_json; 
-     end
+   zoo = Participation.first(:event_id => params[:event_id].to_i)
+   zoo.destroy
+   if zoo.destroyed?
+      return {:status => 'success'}.to_json;
+   else
+      return {:status => 'not_success'}.to_json; 
+   end
 end
 
 post '/participate' do
-	#Participation.create(:event_id => params[:event],:user_id => params[:user_name]);
- zoo = Participation.new
- zoo.attributes = { :event_id => params[:event],:user_id => params[:user_name] }
- zoo.save
+   #Participation.create(:event_id => params[:event],:user_id => params[:user_name]);
+   zoo = Participation.new
+   zoo.attributes = { :event_id => params[:event],:user_id => params[:user_name] }
+   zoo.save
 end
